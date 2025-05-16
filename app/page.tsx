f402413b02057d66d3eba90d47e09603a77ad8ae
yui050505 <yui050505@gmail.com>
2025-05-16 17:43:37 +0000

    firebase studio commit test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return(
-    <div>
+    <div className="flex flex-col item-center justify-between min-h-screen">
       <h1>안녕하세요.</h1>
+      <h1>aaaaa</h1>
     </div>
   )
 }
